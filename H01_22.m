6449bb125dfdce0cd2faa6334a325542fa17a1f5
kelansolo <91731804+kelansolo@users.noreply.github.com>
2021-10-03 17:43:21 +0200

    Add files via upload

---
+++ H01_22.m
@@ -0,0 +1,45 @@
+clear all
+fs_s = 44100;   % sampling frequency [Hz]
+T_s = 5;        % total time[s]
+f0 = 1000;      % fundamental frequency [Hz]
+a_k = 1;        % amplitude
+t_s = 0:1/fs_s:T_s-1/fs_s;
+
+N = 10;         % number of components
+%%
+phase = zeros(1,N); % in the case of zero phase
+ttl="0 phase";
+
+%%
+%Uncomment for pi/2
+% 
+for i=1:N
+    phase(i)= pi/2;
+end
+ttl='pi/2 phase';
+%%
+%Uncomment for random phase
+
+% for i=1:N
+%     phase(i)= rand*2*pi;
+% end
+% ttl='random phase';
+
+%%
+% initialize the signal with a vector of zeros
+s = zeros(1,fs_s*T_s);  % Where does the fs*T come from? -- > number of samples 
+
+% now the loop
+for kk = 1:N
+    s = s + a_k*sin(2*pi*kk*f0*t_s+phase(kk));
+end
+sound(s,fs_s)
+    
+% plotting
+figure('Name','A cool tone complex!')
+
+plot(t_s,s)
+xlabel('t')
+ylabel('y')
+xlim([0 .01])
+title(sprintf('Tone-complex with %s', ttl))
